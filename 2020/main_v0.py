a3eed03e8a4cfc11d8c3c988c839435bacbb5f73
Uncastellum <danicorreo99@gmail.com>
2020-02-20 18:54:46 +0100

    fix v0

--- 2020/main_v0.py
+++ 2020/main_v0.py
@@ -16,15 +16,15 @@ if len(args) != 0:
     file = args[0]
     if file in ['a', 'b', 'c', 'd', 'e', 'f']:
         index = ['a', 'b', 'c', 'd', 'e', 'f'].index(file)
-    else if path in ['1', '2', '3', '4', '5', '6']:
+    elif file in ['1', '2', '3', '4', '5', '6']:
         index = ['1', '2', '3', '4', '5', '6'].index(file)
 
 input = 'input/' + files[index]
 output_file = 'output_' + ['a', 'b', 'c', 'd', 'e', 'f'][index] + '.out'
 
 with open(input) as f_in:
-    # read first line
-    rows, colums, miningr, maxcells = [int(x) for x in next(f_in).split()]
+    #read first line
+    #rows, colums, miningr, maxcells = [int(x) for x in next(f_in).split()]
     matrix = []
     for line in f_in: # read rest of lines
         matrix.append([x for x in line]) #TTTTT
