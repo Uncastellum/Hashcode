3df833a3cb66e841bb0c9231aca15158151c2a04
Uncastellum <danicorreo99@gmail.com>
2020-02-20 19:21:34 +0100

    base base

--- 2020/main_v0.py
+++ 2020/main_v0.py
@@ -22,24 +22,49 @@ if len(args) != 0:
 input = 'input/' + files[index]
 output_file = 'output_' + ['a', 'b', 'c', 'd', 'e', 'f'][index] + '.out'
 
+class Libary(object):
+    """docstring for Libary."""
+    def __init__(self, id, total_b, singup, books_day, books):
+        self.id = id
+        self.total_b = total_b
+        self.singup = singup
+        self.books_day = books_day
+        self.books = books
+
+    def __str__(self):
+        string = '{0} {1} {2}\n{3}\n'
+        return string.format(
+            self.total_b,
+            self.singup,
+            self.books_day,
+            ''.join('%s ' % i for i in self.books)
+        )
+
 with open(input) as f_in:
     #read first line
-    #rows, colums, miningr, maxcells = [int(x) for x in next(f_in).split()]
-    matrix = []
+    books_norep, libs, days = [int(x) for x in next(f_in).split()]
+    books_score = [int(x) for x in next(f_in).split()]
+    all_libs = []
+    id = 0
     for line in f_in: # read rest of lines
-        matrix.append([x for x in line]) #TTTTT
+        b, d, s = [int(x) for x in line.split()]#next(f_in).split()]
+        books = [int(x) for x in next(f_in).split()]#next(f_in).split()]
+        all_libs.append(Libary(id,b,d,s,books))
+        id += 0
+        #matrix.append([x for x in line]) #TTTTT
 
 
 def resulttofile(res):
     with open(output_file, 'w') as f_out:
         for item in res:
-            for subitem in item:
-                f_out.write("%s " % subitem)
+            f_out.write("%s" % item)
             f_out.write("\n")
+            #for subitem in item:
 
 
 def main():
-    resulttofile(matrix)
+    print(len(all_libs))
+    resulttofile(all_libs)
 
 
 if __name__ == '__main__':
